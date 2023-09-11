e9030921b7e449274a9607b4cc563517142ee8c3
Rikuto <orikutobasuke@gmail.com>
2023-09-11 11:48:22 +0900

    commit

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('telNumber');
 const btn = document.getElementById('btn');
-console.log(input, btn);
+const errar = document.querySelector('.errar');
+console.log(input, btn, errar);
 console.log(input.value);
 
 // 何が入力されたかを検知する関数
@@ -25,11 +26,23 @@ function InputTextCheck() {
     }
 }
 
-btn.addEventListener('click', () => {
-    console.log('honnda');
-})
+// 電話番号の入力が正しいかを確認する関数
+function WatchTel() {
+    console.log('発火');
+    let telNum = input.value
+    if (telNum.length < 11) {
+        errar.classList.add('errarOn')
+    }
+    else if (telNum.length === 11) {
+        btn.setAttribute('onclick', "location.href='./certification.html'")
+    }
+    else {
+        errar.classList.add = 'errarOn'
+    }
+}
 
 // 関数を検知するイベント
 input.addEventListener('keyup', InputTextCheck)
 
+btn.addEventListener('click', WatchTel)
 
