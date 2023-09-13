2a9a70cfacc715b3e1f7b21586fb7d0038c844c6
Rikuto <orikutobasuke@gmail.com>
2023-09-13 16:30:20 +0900

    commit

--- src/js/certification.js
+++ src/js/certification.js
@@ -1,3 +1,20 @@
 const inputs = document.querySelectorAll('.inText');
 console.log(inputs);
 
+function setNextFocus(obj) {
+    if (obj.value.length >= obj.maxLength) {
+        var textinput = document.codeNumber.elements;
+        // ループでinputのカウント
+        for (var i = 0; i < textinput.length; i++) {
+            if (textinput[i] == obj) {
+                textinput[i + 1].focus();
+                // 繰り返し処理を終了
+                break;
+            }
+        }
+    }
+}
+
+function Test() {
+
+}
