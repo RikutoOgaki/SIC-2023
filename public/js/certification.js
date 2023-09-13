92ddd7ae1ba7267dfc3370c04aefb672a62db3f5
Rikuto <orikutobasuke@gmail.com>
2023-09-13 13:46:44 +0900

    commit

--- public/js/certification.js
+++ public/js/certification.js
@@ -1,14 +1,2 @@
 const inputs = document.querySelectorAll('.inText');
 console.log(inputs);
-
-function setNubmer(ele) {
-    if (ele.value.length === ele.max)
-        for (i = 0; i < inputs.length; i++) {
-            if (inputs[i].value.length === 1) {
-                inputs[i + 1].focus();
-                break
-            }
-        }
-}
-
-
